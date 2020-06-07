02b217025c9d6da851706957635e62a399005976
Lepticidio <miguel.andujar@live.u-tad.com>
2020-06-07 19:11:56 +0200

    FirstExerciseDone

--- Practica1/scripting/pacman/pacman/main.cpp
+++ Practica1/scripting/pacman/pacman/main.cpp
@@ -5,6 +5,17 @@
 #include "lualib.h"
 
 
+struct Color
+{
+	float r;
+	float g;
+	float b;
+
+	Color(float _r, float _g, float _b) : r(_r), g(_g), b(_b)
+	{
+
+	}
+};
 bool m_bLuaInitialized = false;
 int num_coins = 0;
 const int platas_para_oro = 5;
@@ -15,8 +26,21 @@ float vida = max_vida;
 
 lua_State* m_pLua;
 
-int m_iCoinScore = 0, m_iScoreToBronze = 0;
+int m_iCoinScore = 0, m_iPowerupScore = 0, m_iScoreToBronze = 0;
 float m_fPowerupDuration = 0, m_fSpeedMultiplicator = 0;
+Color m_oPowerupColor(0, 0, 0);
+
+Color ColorFromLua(int _iIndex)
+{
+	lua_getfield(m_pLua, _iIndex, "r");
+	float fRed = lua_tonumber(m_pLua, -1) * 255;
+	lua_getfield(m_pLua, _iIndex, "g");
+	float fGreen = lua_tonumber(m_pLua, -1) * 255;
+	lua_getfield(m_pLua, _iIndex, "b");
+	float fBlue = lua_tonumber(m_pLua, -1) * 255;
+
+	return Color(fRed, fGreen, fBlue);
+}
 
 
 
@@ -33,16 +57,20 @@ bool InitializeLua()
 	}
 	else
 	{
-
-		lua_getglobal( m_pLua,  "coin_score");
+		lua_getglobal(m_pLua, "coin_score");
+		lua_getglobal( m_pLua,  "powerup_score");
 		lua_getglobal(m_pLua, "score_to_bronze");
 		lua_getglobal(m_pLua, "powerup_duration");
 		lua_getglobal(m_pLua, "speed_multiplicator");
+		lua_getglobal(m_pLua, "powerup_color");
 
 		m_iCoinScore = lua_tointeger(m_pLua, 1);
-		m_iScoreToBronze = lua_tointeger(m_pLua, 2);
-		m_fPowerupDuration = lua_tonumber(m_pLua, 3);
-		m_fSpeedMultiplicator = lua_tonumber(m_pLua, 4);
+		m_iPowerupScore = lua_tointeger(m_pLua, 2);
+		m_iScoreToBronze = lua_tointeger(m_pLua, 3);
+		m_fPowerupDuration = lua_tonumber(m_pLua, 4);
+		m_fSpeedMultiplicator = lua_tonumber(m_pLua, 5);
+
+		m_oPowerupColor = ColorFromLua(6);
 
 		return true;
 	}
@@ -52,6 +80,8 @@ void FinalizeLua()
 {
 	lua_close(m_pLua); /* cierra el entorno */
 }
+
+
 bool pacmanEatenCallback(int& score, bool& muerto)
 { // Pacman ha sido comido por un fantasma
 	vida -= 0.5f;
@@ -81,10 +111,10 @@ bool ghostEatenCallback(int& score)
 bool powerUpEatenCallback(int& score)
 { // Pacman se ha comido un powerUp
 	setPacmanSpeedMultiplier(m_fSpeedMultiplicator);
-	setPacmanColor(0, 255, 0);
+	setPacmanColor(m_oPowerupColor.r, m_oPowerupColor.g, m_oPowerupColor.b);
 	setPowerUpTime(m_fPowerupDuration);
 
-	score += 5000;
+	score += m_iPowerupScore;
 
 	return true;
 }
